c7d665e1655152766ce05a44046ef8e1cb380878
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-25 13:21:41 +0700

    delete settings

--- bot_settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','1588614428');
-define('LINE_MESSAGE_CHANNEL_SECRET','4d2466fe67c5096d309d0ac512c245e7');
-define('LINE_MESSAGE_ACCESS_TOKEN','DmeTxctmIRhMZkN9HXYRKN6a4iiSy+bAK86dHqxQ5L6GQZpOSIWfnj3xKDdqkvGDKivRIKFmtS43Xfw8uZfsNN/6kg/qiCZt2yF7Ve0+3YfAvsD4gCA+GqI0Vo9MTZVMsUDenNMta5m6itAVgcFl/AdB04t89/1O/w1cDnyilFU=');
-?>
