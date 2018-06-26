0353a63a8fc49526423a4b167db6a17744ca0c51
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-26 19:16:14 +0700

    add button

--- FaceBookBot.php
+++ FaceBookBot.php
@@ -1,32 +1,63 @@
 <?php
-// parameters
-$hubVerifyToken = 'tk_nom_sod';
-$accessToken = "EAAbHInA6V4kBANAZCPWtdl020HcGVNlZBZAHSddhfYQmcapPNdIZCmcIZAbH0rQqon8nyuZCWR2eKRieyA3w5Syn9n97pPKyZCgrLXefPPCZB4G43LEW3XDYupu2iyMkfpgVq9pZATRBRxA5CH71e0IKpQSdZA9o6CZAq0ymYizzkwDo2i2nH3ckFTL";
-// check token at setup
-if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-  echo $_REQUEST['hub_challenge'];
-  exit;
-}
-// handle bot's anwser
-$input = json_decode(file_get_contents('php://input'), true);
-$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
-$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-$response = null;
-//set Message
-if($messageText == "hi") {
-    $answer = "Hello";
-}
-//send message to facebook bot
-$response = [
+  // parameters
+  $hubVerifyToken = 'tk_nom_sod';
+  $accessToken = "EAAbHInA6V4kBANAZCPWtdl020HcGVNlZBZAHSddhfYQmcapPNdIZCmcIZAbH0rQqon8nyuZCWR2eKRieyA3w5Syn9n97pPKyZCgrLXefPPCZB4G43LEW3XDYupu2iyMkfpgVq9pZATRBRxA5CH71e0IKpQSdZA9o6CZAq0ymYizzkwDo2i2nH3ckFTL";
+  // check token at setup
+  if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+    echo $_REQUEST['hub_challenge'];
+    exit;
+  }
+  // handle bot's anwser
+  $input = json_decode(file_get_contents('php://input'), true);
+  $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+  $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+  $response = null;
+  //set Message
+  if($messageText == "hi") {
+      $answer = "Hello";
+  }
+  //send message to facebook bot
+  $response = [
+      'recipient' => [ 'id' => $senderId ],
+      'message' => [ 'text' => $answer ]
+  ];
+  if($messageText == "blog"){
+     $answer = ["attachment"=>[
+      "type"=>"template",
+      "payload"=>[
+        "template_type"=>"generic",
+        "elements"=>[
+          [
+            "title"=>"Welcome to Peter\'s Hats",
+            "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+            "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+            "subtitle"=>"We\'ve got the right hat for everyone.",
+            "buttons"=>[
+              [
+                "type"=>"web_url",
+                "url"=>"https://petersfancybrownhats.com",
+                "title"=>"View Website"
+              ],
+              [
+                "type"=>"postback",
+                "title"=>"Start Chatting",
+                "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
+              ]              
+            ]
+          ]
+        ]
+      ]
+    ]];
+     $response = [
     'recipient' => [ 'id' => $senderId ],
-    'message' => [ 'text' => $answer ]
-];
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
-$result = curl_exec($ch);
-}
-curl_close($ch);
+    'message' => $answer 
+  ];}
+  $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+  if(!empty($input)){
+  $result = curl_exec($ch);
+  }
+  curl_close($ch);
 ?>
