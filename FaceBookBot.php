a97638beab4119f90624a5ec19f45f898cb882d6
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-27 16:46:49 +0700

    edyt respond message

--- FaceBookBot.php
+++ FaceBookBot.php
@@ -54,11 +54,11 @@
 NOSTRA Hotline Service
 (66)2 266 9940
  nostrahotline@cdg.co.th"
-        $response = [
+    }
+    $response = [
           'recipient' => [ 'id' => $senderId ],
           'message' => $answer
         ];
-    }
 
   $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
     curl_setopt($ch, CURLOPT_POST, 1);
