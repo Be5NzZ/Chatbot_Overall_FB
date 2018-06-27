f6d971029e71ea4f267bcf8cae242d275b91402e
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-27 13:28:42 +0700

    delete pic

--- FaceBookBot.php
+++ FaceBookBot.php
@@ -46,10 +46,7 @@
       'message' => $answer
   ];
   }
-  if($messageText == "pic") {
-      $image_url = "https://www.img.in.th/images/2457764ef43d1fb1dffdc577a982c2a6.jpg",
-      $answer = $image_url;
-  }
+
   //send message to facebook bot
   $response = [
       'recipient' => [ 'id' => $senderId ],
