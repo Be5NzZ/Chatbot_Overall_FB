f005e263dec3fa0bba84773057a3023cf4246a81
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-27 16:14:01 +0700

    add sender_acction

--- FaceBookBot.php
+++ FaceBookBot.php
@@ -58,12 +58,15 @@
         ];
     }
 
-  $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-  if(!empty($input)){
-  $result = curl_exec($ch);
-  }
+  $ch = [
+    "sender_action" => "typing_on", 
+    curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    if(!empty($input)){
+      $result = curl_exec($ch);
+    }
+  ]
   curl_close($ch);
 ?>
