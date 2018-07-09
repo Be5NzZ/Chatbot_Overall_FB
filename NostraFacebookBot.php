08ba60408d7a9ceccc96aeb6bc0c5e0e584b234a
yyingz <40383818+yyingz@users.noreply.github.com>
2018-07-09 16:04:34 +0700

    edit condition

--- NostraFacebookBot.php
+++ NostraFacebookBot.php
@@ -29,7 +29,7 @@
     ];
   }
 
-  if($messageText == "ข้อมูลติดต่อทีมงานโดยตรง") {
+  if($messageText == "ข้อมูลติดต่อ (Contact Information)") {
       $answer = "NOSTRA Hotline Service
 (66)2 266 9940
  nostrahotline@cdg.co.th";
@@ -40,7 +40,7 @@
       ];
   }
 
-  if($messageText == "แสดงความคิดเห็นเพิ่มเติมเกี่ยวกับแอพลิเคชัน") {
+  if($messageText == "แสดงความคิดเห็นเกี่ยวกับแอปพลิเคชัน (Application Feedback)") {
       $answer = "ขอบคุณมากครับ รบกวนแสดงความคิดเห็นของท่านได้เลยครับ :)";
       //send message to facebook bot
       $response = [
